e2fc81886eafc20c0cba884ba6bd92a41675e868
imronpuji <imronpuji5@gmail.com>
2022-09-20 13:19:29 +0000

    main

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>views</h1>
+
+  <User/>
+
+</template>
+
+<script>
+
+import User from "./views/User.vue"
+import Client from "../client.vue"
+
+// panggil halaman views dari difolder mana / halaman apa 
+
+export default {
+  
+  components: {
+    User,
+    Client,
+  }
+}
+</script>
+
---
+++ src/views/User.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1> halaman users / data user </h1>
+  <h1>{{user}}</h1>
+</template>
+
+<script>
+ export default {
+    data(){
+      return {
+        users : ""
+      }
+    },
+
+    created(){
+      console.log("ini lifecycle created")
+      this.users = "imron"
+    }
+ }
+</script>
